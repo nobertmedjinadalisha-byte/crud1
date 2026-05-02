b5ed3b723e15e2ecd6bd859bdf9d666158ccb4c8
nobertmedjinadalisha-byte <nobertmedjinadalisha@gmail.com>
2026-05-02 17:10:45 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header style="background:#333; color:white; padding:10px;">
+    <h1>Meu Sistema CRUD</h1>
+</header>
+
+<hr>
