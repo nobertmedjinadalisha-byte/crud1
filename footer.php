b3b01356b284cea01928629f121a70edbd7cf10c
nobertmedjinadalisha-byte <nobertmedjinadalisha@gmail.com>
2026-05-02 16:54:32 -0300

    Enhance footer with background color and padding
    Updated footer styling and structure.

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
 <hr>
 
-<footer>
+<footer style="background:#333; color:white; padding:10px; text-align:center;">
     <p>© 2026 - Meu CRUD</p>
 </footer>
 
-<script src="script.js"></script>
 </body>
 </html>
